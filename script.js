dce98fdf8e6b15ac4bb7f41c6344d1efc67c3af9
Igor Gorovyy <igorgorovoy@ukr.net>
2019-07-22 20:56:48 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+c: number;
+c = add(2, 3);
+alert(c);
---
+++ script.ts
@@ -0,0 +1,7 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+c = add(2, 3);
+
+alert(c);
